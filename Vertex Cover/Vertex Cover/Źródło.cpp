6bcbc3e551d7a47579e1a5ed7ebba7740051941e
Filip Mikołajek <s16586@pjwstk.edu.pl>
2019-11-26 15:36:36 +0100

    Aktualizacja-5 (Generating graph, climbing algorithm)

--- Vertex Cover/Vertex Cover/Źródło.cpp
+++ Vertex Cover/Vertex Cover/Źródło.cpp
@@ -8,6 +8,7 @@
 #include<string>
 #include<random>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -39,14 +40,13 @@ void Show_conections()
 
 float Best_solution(vector<int> solution, int number_of_vertex, int number_of_edges) //solution contains selected vertexes
 {
-	int edges_in_soluton=0;
-	int vertex_in_solution = 0;
-	int number_of_duplicats=0;
+	int edges_in_soluton=0, vertex_in_solution = 0, number_of_duplicats = 0,b;
+	float current_solution=0;
 	vector<int> entered_edges;
-
+	//how many vertexes and edges
 	for (int x=0; x < solution.size(); x++) {
 		cout << "SOLUTIONS:" << solution[x] << endl;
-		int b = solution[x];
+		b = solution[x];
 		b--;
 		for (int y = 0; y < h[b].size(); y++)//for evry edge in x 
 		{
@@ -76,16 +76,20 @@ float Best_solution(vector<int> solution, int number_of_vertex, int number_of_ed
 	}
 
 	cout << "V=" << vertex_in_solution << " E=" << edges_in_soluton<<" D="<< number_of_duplicats <<endl;
-	best_solution += (float)number_of_vertex / (float)vertex_in_solution;//points for number of  used vertex
-	best_solution *= ((float)number_of_edges * pow(((float)edges_in_soluton-(float)number_of_duplicats), 2.0));//points for covering edges
-	best_solution -= (pow(number_of_duplicats, 2.0) * 4);//minus points for duplicats
-	cout <<"best_solution= "<<best_solution<<endl;
-	return best_solution;
+	current_solution += (float)number_of_vertex / (float)vertex_in_solution;//points for number of  used vertex
+	current_solution *= ((float)number_of_edges * pow(((float)edges_in_soluton-(float)number_of_duplicats), 2.0));//points for covering edges
+	current_solution -= (pow(number_of_duplicats, 2.0) * 4);//minus points for duplicats
+	if (current_solution > best_solution) {
+		best_solution= current_solution;
+		cout << "best_solution= " << best_solution << endl;
+	}
+	return current_solution;
 }
 
 void Climbing_Algorythm(int number_of_vertex, int number_of_edges)
 {
-	vector<int>random_solution; 
+	float current_solution;
+	vector<int>random_solution;
 	random_device rd; // obtain a random number from hardware
 	mt19937 eng(rd()); // seed the generator
 	uniform_int_distribution<> distr(1, number_of_vertex);// define the range
@@ -95,9 +99,53 @@ void Climbing_Algorythm(int number_of_vertex, int number_of_edges)
 	}
 	//random_solution.push_back(1);
 	//random_solution.push_back(1);
-	Best_solution(random_solution, number_of_vertex, number_of_edges);//rate random solution
-	//ALGORYTM 
-	//algorythm, rate every new solution
+	for (int x = random_solution.size(); x < random_solution.size() * 2; x++)//alternatywna 1
+	{
+		current_solution=Best_solution(random_solution, number_of_vertex, number_of_edges);//rate random solution
+
+		if (current_solution == best_solution)
+		{
+			if (random_solution[0]< number_of_vertex)
+			{
+			random_solution[0] = random_solution[0] + 1;
+			}
+		}
+		if(current_solution<best_solution)
+		{
+			x = random_solution.size() * 2;
+		}
+	}
+	/*if (random_solution.size() > 1)//alternatywna 2
+	{
+		int range=2,new_solution,start_solution;
+		uniform_int_distribution<> close_solution(1, range);
+		current_solution = Best_solution(random_solution, number_of_vertex, number_of_edges);//rate random solution
+		start_solution = random_solution[0];
+		for (int x = 0; x < range; x++)
+		{
+			new_solution = close_solution(eng);
+			if (new_solution % 2 == 0) 
+			{
+				if (new_solution + random_solution[0] < random_solution.size())
+				{
+					random_solution[0] += new_solution;
+					current_solution = Best_solution(random_solution, number_of_vertex, number_of_edges);//rate random solution
+					random_solution[0] = start_solution;
+				}
+			}
+			else {
+				if ( random_solution[0] - new_solution > 1)
+				{
+					random_solution[0] -= new_solution;
+					current_solution = Best_solution(random_solution, number_of_vertex, number_of_edges);//rate random solution
+					random_solution[0] = start_solution;
+				}
+			}
+		}
+	}
+	else { current_solution = Best_solution(random_solution, number_of_vertex, number_of_edges); }*/
+	
+	cout << "Best solution for climbing algorythm=" << best_solution << endl;
 }
 
 /*vector<int>void BruteForceVertexCover(int number_of_vertex, int number_of_edges)
@@ -106,7 +154,6 @@ void Climbing_Algorythm(int number_of_vertex, int number_of_edges)
 //jeœli ¿aden pojednyñcy nie pokrywa to sprawdzam dla vertex 1 + vertex 2 pó¿niej 1 + 3 itd.
 //edge 1-2 == 2-1 !!!
 
-	
 	//OCENA ROZWI¥ZANIA W FUNCJI CELU
 	
 				//krawêdŸ 1-2==krawêdŸ 2-1 przez co nie doda jej do listy zaznaczonych krawêdzi która musi mieæ w sobie number_of_edges krawêdzi
@@ -115,25 +162,6 @@ void Climbing_Algorythm(int number_of_vertex, int number_of_edges)
 		//drugi vektor 2d? 1d? sprawdzaj¹cy czy krawêdzie zaznaczone
 		// wyœlij kazde rozwi¹zanie ale jeœli jest s³abe to dostanie s³ab¹ ocene w funkcji celu
 }*/
-
-vector<int> sol_vertex(int n, int e) {
-	vector<int> S;
-	for (int i = 0; i < n; i++) {
-		if (!v[i]) {
-			for (int j = 0; j < (int)g[i].size(); j++) {
-				if (!v[g[i][j]]) {
-					v[i] = true;
-					v[g[i][j]] = true;
-					break;
-				}
-			}
-		}
-	}
-	for (int i = 0; i < n; i++)
-		if (v[i])
-			S.push_back(i);
-	return S;
-}
 
 tuple<int, int> data_from_file(int& number_of_vertex, int& number_of_edges, int conection_start, int conection_end)
 {
@@ -170,16 +198,35 @@ tuple<int, int> data_from_file(int& number_of_vertex, int& number_of_edges, int 
 void data_to_file(int number_of_vertex, int number_of_edges)
 {
 	fstream output_data;
+	output_data << "-------------------------------------------------------------------------------------------" << endl;
 	output_data.open("output_data.txt", ios::out | ios::app);
 	output_data << "Numer of vertex: " << number_of_vertex << endl;
 	output_data << "Number of edges: " << number_of_edges << endl;
-	
+	output_data << "Example: X---edge_nr---Y" << endl;
+	for (int x = 0; x < g.size(); x++)//cout conections
+	{
+		output_data << "NR_X=" << x + 1 << "  NR_Y:";
+		for (int y = 0; y < g[x].size(); y++)
+		{
+			output_data << g[x][y] + 1 << "-";
+		}
+		output_data << endl;
+		output_data << "NR_X=" << x + 1 << " include Edges_NR:";
+		for (int y = 0; y < h[x].size(); y++)
+		{
+			output_data << h[x][y] + 1 << "-";
+		}
+		output_data << endl;
+	}
+	output_data <<"-------------------------------------------------------------------------------------------"<<endl;
 	output_data.close();
 }
 
-/*tuple<int,int,int,int> generate_graph()
+tuple<int,int> generate_graph(int& number_of_vertex, int& number_of_edges, int conection_start, int conection_end)
 {
-	int number_of_vertex, number_of_edges, conection_start, conection_end;
+	random_device rd; // obtain a random number from hardware
+	mt19937 eng(rd()); // seed the generator
+
 	cout << "enter number of vertex: " ;
 	cin >> number_of_vertex;
 		if (number_of_vertex<2)
@@ -187,75 +234,58 @@ void data_to_file(int number_of_vertex, int number_of_edges)
 			cout << "number of vertex > 2 !";
 			exit(0);
 		}
-	number_of_edges = rand() % ((number_of_vertex * (number_of_vertex - 1)) / 2);
+		
+	uniform_int_distribution<> random_nr_of_edges(1, number_of_vertex *(number_of_vertex - 1)/2);//no loops
+	number_of_edges = random_nr_of_edges(eng);
 	cout << "The graph has " << number_of_edges << " edges."<<endl;
 	
 	g.resize(number_of_vertex);
-	memset(v, 0, sizeof(v));
+	h.resize(number_of_vertex);
 	int current_start, current_end;
-	for (int x = 0; x < number_of_edges; x++) {
-		current_start = rand() % number_of_vertex + 1;
-		current_end = rand() % number_of_vertex + 1;
-		if (current_end == current_start) 
+	uniform_int_distribution<> random_edges(1, number_of_vertex);
+	for (int x = 0; x < number_of_edges; x++) 
+	{
+		current_start = random_edges(eng);
+		current_end = random_edges(eng);
+
+		if (current_end == current_start)
 		{
-			current_end++;
-		}
-		if(find(g.begin(), g.end(), current_start) != g.end())
-		{
-			if (find(g.begin(), g.end(), current_end) != g.end())
+			if (current_end >= number_of_vertex)
 			{
-				// elem exists in the vector
+				current_end--;
+			}
+			if (current_end < number_of_vertex)
+			{
+				current_end++;
 			}
 		}
-		else {
-			current_start--; current_end--;
-			g[current_start].push_back(current_end);
-			g[current_end].push_back(current_start);
-		}
-		g[x].push_back(current_edge);
-		g[current_edge].push_back(x);
-		cout << g[x][current_edge];
-		current_edge--;*
+		
+		current_start--; current_end--;
+		g[current_start].push_back(current_end);
+		g[current_end].push_back(current_start);
+
+		h[current_start].push_back(x);
+		h[current_end].push_back(x);
 	}
-	return { number_of_vertex, number_of_edges, conection_start, conection_end };
-}*/
+	return { number_of_vertex, number_of_edges };
+}
 
 int main() {
-	clock_t start = clock();
+	//clock_t start = clock();
 	//printf("Czas wykonywania: %lu ms\n", clock() - start);
 	int number_of_vertex=0, number_of_edges=0, conection_start=0, conection_end=0, minimum_vertex_number=0;
 
-	data_from_file(number_of_vertex, number_of_edges, conection_start, conection_end);
-	//tie(number_of_vertex, number_of_edges)= data_from_file(number_of_vertex, number_of_edges,conection_start , conection_end);
-	//tie(number_of_vertex, number_of_edges, conection_start, conection_end)= generate_graph();
+	//data_from_file(number_of_vertex, number_of_edges, conection_start, conection_end);
+	generate_graph(number_of_vertex, number_of_edges, conection_start, conection_end);
 	
 	//cout <<"OOOOOOOOOOOOOOOOOOOOOOOOOOO=="<< g[0][2]<<endl;
-	///*vector<int> solution =*/BruteForceVertexCover(number_of_vertex, number_of_edges);//JESLI dziala to dziala reszta
 	
 	Show_conections();
+
+	clock_t start = clock();
 	Climbing_Algorythm(number_of_vertex, number_of_edges);
+	printf("Czas wykonywania: %lu ms\n", clock() - start);
 
 	data_to_file(number_of_vertex, number_of_edges);
-	
-	//vector<int> solution = sol_vertex(number_of_vertex, number_of_edges);//zrób rozwi¹zanie si³owe
-
-	//best_solution(minimum_vertex_number);//FUNKCJA CELU,
-
-	cout << "The required vertex cover is as follows:\n";//Exit
-	for (int i = 0; i < (int)solution.size(); i++) {
-		cout << solution[i] + 1 << " ";
-		minimum_vertex_number = minimum_vertex_number + 1;
-	}
-	cout << endl;
-	cout <<"Number on needed edges: "<< minimum_vertex_number;
-	minimum_vertex_number = minimum_vertex_number;
-
-	
-	cout << endl <<"minimum vertex=" << minimum_vertex_number;
-	printf("Czas wykonywania: %lu ms\n", clock() - start);
 	return 0;
 }
-
-//brute force- sprawdzanie czy dany vertex zaznacza wszystkie number_of_edges-krawiendzie jeœli nie to nastêpny
-//jeœli ¿aden pojednyñcy nie pokrywa to sprawdzam dla vertex 1 + vertex 2 pó¿niej 1 + 3 itd.
-//edge 1-2 == 2-1 !!!
